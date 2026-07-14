99a86a0995f81fe3096c8d13fbc807689b21a77b
CarolinaCalvi <jcalvi.carolina@gmail.com>
2026-07-14 18:51:56 -0300

    adição do total de offsets existentes na api

--- main.py
+++ main.py
@@ -5,8 +5,26 @@ url = "https://employability-portal.gupy.io/api/v1/jobs?"
 params = {"jobName": "dados"}
 response = requests.get(url, params = params)
 
-# print(response.json())
+# data = response.json()
+
+# print(data.get("pagination"))
 
 data = response.json()
+pagination = data.get("pagination", {})
 
-print(data.get("pagination"))
+total = pagination.get("total", 0)
+limit = pagination.get("limit", 0)
+offset = pagination.get("offset", 0)
+
+
+total_posicoes = total / limit
+if total_posicoes % 1 != 0:
+    total_posicoes = int(total_posicoes) + 1
+
+total_offset = total_posicoes - 1
+
+print("total:", total)
+print("limit:", limit)
+print("offset:", offset)
+print("total_posicoes:", total_posicoes)
+print("total_offset:", total_offset)
