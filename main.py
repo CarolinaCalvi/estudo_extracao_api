8d0b8aa61236f7fad3e10555615e67363fa1f23e
CarolinaCalvi <jcalvi.carolina@gmail.com>
2026-07-15 19:58:08 -0300

    ajuste do arredondamento da métrica total_offset + lopp para trazer  dados da api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import math
 
 url = "https://employability-portal.gupy.io/api/v1/jobs?"
 
@@ -8,8 +9,7 @@ response = requests.get(url, params = params)
 
 data = response.json()
 
-with open("resultado_api.json", "w", encoding="utf-8") as arquivo:
-    json.dump(data, arquivo, ensure_ascii=False, indent=4)
+print("data:", data)
 
 pagination = data.get("pagination", {})
 
@@ -17,12 +17,11 @@ total = pagination.get("total", 0)
 limit = pagination.get("limit", 0)
 offset = pagination.get("offset", 0)
 
-
 total_posicoes = total / limit
-if total_posicoes % 1 != 0:
-    total_posicoes = int(total_posicoes) + 1
+# if total_posicoes % 1 != 0:
+#     total_posicoes = int(total_posicoes) + 1
 
-total_offset = total_posicoes - 1
+total_offset = math.ceil(total_posicoes)
 
 print("total:", total)
 print("limit:", limit)
@@ -30,4 +29,20 @@ print("offset:", offset)
 print("total_posicoes:", total_posicoes)
 print("total_offset:", total_offset)
 
+resultados_offsets = []
 
+for offset_atual in range(0, total_offset):
+    params["offset"] = offset_atual
+
+    response = requests.get(url, params=params)
+    data_offset = response.json()
+    print("data_offset:", data_offset)
+    resultados_offsets.append(data_offset)
+
+with open("resultado_api.json", "w", encoding="utf-8") as arquivo:
+    json.dump(
+        resultados_offsets,
+        arquivo,
+        ensure_ascii=False,
+        indent=4
+    )
